424725250e5ed874f9d919e593b228a91938aa22
Twan L <twanluttik@gmail.com>
2025-08-19 00:52:32 -0700

    fixed types

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,30 @@
+import dts from 'rollup-plugin-dts';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 
-const extensions = ['.js', '.ts', '.tsx'];
+export default [
+	// JS bundles
+	{
+		input: 'src/index.ts',
+		output: [
+			{ file: 'dist/index.cjs', format: 'cjs', sourcemap: true },
+			{ file: 'dist/index.esm.js', format: 'esm', sourcemap: true },
+		],
+		plugins: [
+			resolve({ extensions: ['.js', '.ts', '.tsx'] }),
+			commonjs(),
+			typescript({ tsconfig: './tsconfig.json', declaration: false }),
+			babel({ extensions: ['.js', '.ts', '.tsx'], babelHelpers: 'bundled', presets: ['@babel/preset-react'] }),
+		],
+		external: ['react', 'react-dom'],
+	},
 
-export default {
-	input: 'src/index.ts', // entry point
-	output: [
-		{
-			file: 'dist/index.cjs', // CommonJS build
-			format: 'cjs',
-			sourcemap: true,
-		},
-		{
-			file: 'dist/index.esm.js', // ESM build
-			format: 'esm',
-			sourcemap: true,
-		},
-	],
-	plugins: [
-		resolve({ extensions }), // so Rollup can find node_modules
-		commonjs(), // convert CJS → ESM if needed
-		typescript({ tsconfig: './tsconfig.json' }),
-		babel({
-			extensions,
-			babelHelpers: 'bundled',
-			presets: ['@babel/preset-react'],
-		}),
-	],
-	external: ['react', 'react-dom'], // don’t bundle React
-};
+	// Types
+	{
+		input: 'src/index.ts',
+		output: { file: 'dist/index.d.ts', format: 'es' },
+		plugins: [dts()],
+	},
+];
